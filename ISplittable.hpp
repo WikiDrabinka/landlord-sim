bba6cc6a6105db6c5453335cb83310b8f9023d0b
Wiktoria Drabinska <wiktoria.drabinska@gmail.com>
2024-10-31 15:49:12 +0100

    Changed structure from .hpp files to .h and .cpp

--- ISplittable.hpp
+++
@@ -1,9 +0,0 @@
-#include <memory>
-namespace splittable {
-    class ISplittable {
-        public:
-        virtual bool containsPoint(int x, int y) = 0;
-        void merge(std::shared_ptr<ISplittable> newSplittable);
-        std::shared_ptr<ISplittable> split();
-    };
-}
--- main.cpp
+++ main.cpp
@@ -1,26 +1,26 @@
-#include "apartment.hpp"
+#include "room.h"
 #include <vector>
 int main() {
     tenant::Tenant *bob = new tenant::Tenant();
-    // std::vector<walls::Wall *> roomWalls;
-    std::shared_ptr<walls::Wall> wall1 = std::shared_ptr<walls::Wall>(new walls::Wall(0,0,0,9));
-    std::shared_ptr<walls::Wall> mergedWall = wall1->split(0,5);
+    // std::vector<wall::Wall *> roomWalls;
+    std::shared_ptr<wall::Wall> wall1 = std::shared_ptr<wall::Wall>(new wall::Wall(0,0,0,9));
+    std::shared_ptr<wall::Wall> mergedWall = wall1->split(0,5);
     wall1->merge(mergedWall);
-    // walls::Wall *wall2 = new walls::Wall(0,0,3,0);
-    // walls::Wall *wall4 = new walls::Wall(3,0,3,4);
-    // walls::Wall *wall5 = new walls::Wall(0,9,5,9);
-    // walls::Wall *wall6 = new walls::Wall(3,4,5,4);
-    // walls::Wall *wall7 = new walls::Wall(5,9,5,4);
+    // wall::Wall *wall2 = new wall::Wall(0,0,3,0);
+    // wall::Wall *wall4 = new wall::Wall(3,0,3,4);
+    // wall::Wall *wall5 = new wall::Wall(0,9,5,9);
+    // wall::Wall *wall6 = new wall::Wall(3,4,5,4);
+    // wall::Wall *wall7 = new wall::Wall(5,9,5,4);
     // roomWalls.push_back(wall1);
     // roomWalls.push_back(wall2);
     // roomWalls.push_back(wall4);
     // roomWalls.push_back(wall5);
     // roomWalls.push_back(wall6);
     // roomWalls.push_back(wall7);
-    // walls::Door *door1 = new walls::Door(0,2);
-    // std::vector<walls::Door *> doors;
+    // wall::Door *door1 = new wall::Door(0,2);
+    // std::vector<wall::Door *> doors;
     // doors.push_back(door1);
-    // apartment::Room *room = new apartment::Room("Bedroom",roomWalls,doors, livingSpace::unclaimed);
+    // room::Room *room = new room::Room("Bedroom",roomWalls,doors, livingSpace::unclaimed);
     // //bob->sign(room,500,90);
     std::shared_ptr<item::Item> item1 = std::shared_ptr<item::Item>(new item::Item());
     std::cout << *item1;
@@ -29,7 +29,9 @@ int main() {
     furniture::Storage *shelf = new furniture::Storage("hi",100,100,1,1,3);
     shelf->addItem(item1);
     std::cout << *shelf;
-    delete bob;
+    delete bob; 
     delete shelf;
     return 0;
 }
+
+//TO DO : test merging rooms, write splitting rooms, checking if point is in room, placing furniture, create class apartment
